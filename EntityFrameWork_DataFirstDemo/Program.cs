56d72627df590dabc05bf817485ff62e1f1e39bd
Jess Yu <jessyu0106@gmail.com>
2018-06-30 11:32:55 -0400

    Database First Demo

---
+++ EntityFrameWork_DataFirstDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameWork_DataFirstDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //connect to ADO.NET EF
+            var db = new DatabaseFirstDemoEntities();
+            //create post
+            var post = new Post()
+            {
+                Body = "body",
+                DatePublished = DateTime.Now,
+                Title = "title",
+                PostID = 1
+            };
+            db.Posts.Add(post);
+            db.SaveChanges();
+        }
+    }
+}
